bb1a00d28bb36c0919968bb1ee02510dccd3caf0
Justin Latimer <justin@doublemu.com>
2011-08-05 00:55:48 +1000

    Add a test that uses a virtual input and virtual output to make a loopback.

---
+++ test/virtual-loopback-test.js
@@ -0,0 +1,19 @@
+var midi = require("../build/default/midi.node");
+
+var output = new midi.output();
+var input = new midi.input();
+
+output.openVirtualPort("node-midi Virtual Output");
+input.on('message', function(deltaTime, message) {
+  console.log('m:' + message + ' d:' + deltaTime);
+  output.sendMessage([
+    message[0],
+    message[1] + 10,
+    message[2]
+  ]);
+});
+input.openVirtualPort("node-midi Virtual Input");
+setTimeout(function() {
+  input.closePort();
+  output.closePort();
+}, 60000);

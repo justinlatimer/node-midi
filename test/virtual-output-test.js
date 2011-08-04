2a11830eee806eeb8ce2a6646d0b2ec9e92f8e2c
Justin Latimer <justin@doublemu.com>
2011-08-04 23:24:58 +1000

    Update the virtual output test file to take some time between messages so the results are visible.

--- test/virtual-output-test.js
+++ test/virtual-output-test.js
@@ -2,8 +2,12 @@ var midi = require("../build/default/midi.node");
 
 var output = new midi.output();
 output.openVirtualPort("node-midi Virtual Output");
-output.sendMessage([176,22,1]);
-output.sendMessage([176,22,99]);
+setTimeout(function() {
+  output.sendMessage([144,64,90]);
+}, 10000);
+setTimeout(function() {
+  output.sendMessage([128,64,40]);
+}, 15000);
 setTimeout(function() {
   output.closePort();
-}, 10000);
+}, 20000);

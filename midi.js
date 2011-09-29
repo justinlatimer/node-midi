6477835bf146797393df98ef8d94fc15b50374a7
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2011-09-29 06:55:25 +0200

    workaround for midi_addon location

--- midi.js
+++ midi.js
@@ -1,4 +1,13 @@
 var EventEmitter = require('events').EventEmitter;
-var midi = require('./build/default/midi_addon');
+try {
+    var midi = require('./build/Release/midi_addon');
+} catch(e) {
+    try {
+        var midi = require('./build/default/midi_addon');
+    } catch(e2) {
+        console.log(e);
+        throw(e2);
+    }
+}
 midi.input.prototype.__proto__ = EventEmitter.prototype;
 module.exports = midi;

825064d88724422ce3b385932c1683e33fb67564
Justin Latimer <justin@doublemu.com>
2012-01-08 10:24:38 +1000

    TIdy up main script file and add comments.

--- midi.js
+++ midi.js
@@ -1,13 +1,14 @@
-var EventEmitter = require('events').EventEmitter;
+// Node version 0.4.0 and 0.6.0 compatibility
 try {
-    var midi = require('./build/Release/midi_addon');
+  var midi = require('./build/Release/midi_addon');
+} catch(e) { try {
+  var midi = require('./build/default/midi_addon');
 } catch(e) {
-    try {
-        var midi = require('./build/default/midi_addon');
-    } catch(e2) {
-        console.log(e);
-        throw(e2);
-    }
-}
+  throw e;
+}}
+
+// MIDI input inherits from EventEmitter
+var EventEmitter = require('events').EventEmitter;
 midi.input.prototype.__proto__ = EventEmitter.prototype;
+
 module.exports = midi;

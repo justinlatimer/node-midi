14a1909a4e42013d2c9ac18b063f788cf5a16e96
andrew morton <drewish@katherinehouse.com>
2014-01-31 23:46:25 -0800

    Added some tests for getPortName

--- test/unit-tests.js
+++ test/unit-tests.js
@@ -4,7 +4,7 @@ var Midi = require('../midi');
 
 describe('midi.input', function() {
   it('should raise when not called with new', function() {
-    (function(){
+    (function() {
       Midi.input();
     }).should.throw('Use the new operator to create instances of this object.');
   });
@@ -21,11 +21,31 @@ describe('midi.input', function() {
       input.getPortCount().should.be.within(0, 255);
     });
   });
+
+  describe('.getPortName', function() {
+    var input = new Midi.input();
+
+    it('requires an argument', function() {
+      (function() {
+        input.getPortName();
+      }).should.throw('First argument must be an integer');
+    });
+
+    it('requires an integer', function() {
+      (function() {
+        input.getPortName('asdf');
+      }).should.throw('First argument must be an integer');
+    });
+
+    it('returns an empty string for unknown port', function() {
+      input.getPortName(999).should.eql('');
+    });
+  });
 });
 
 describe('midi.output', function() {
   it('should raise when not called with new', function() {
-    (function(){
+    (function() {
       Midi.output();
     }).should.throw('Use the new operator to create instances of this object.');
   });
@@ -40,6 +60,26 @@ describe('midi.output', function() {
     it('should return an integer', function() {
       // I feel like having more than 255 ports indicates a problem.
       output.getPortCount().should.be.within(0, 255);
+    });
+  });
+
+  describe('.getPortName', function() {
+    var output = new Midi.output();
+
+    it('requires an argument', function() {
+      (function() {
+        output.getPortName();
+      }).should.throw('First argument must be an integer');
+    });
+
+    it('requires an integer', function() {
+      (function() {
+        output.getPortName('asdf');
+      }).should.throw('First argument must be an integer');
+    });
+
+    it('returns an empty string for unknown port', function() {
+      output.getPortName(999).should.eql('');
     });
   });
 });

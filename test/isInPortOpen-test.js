3235361074392e54f8bcee1aab26f666b59061cb
nroadley <hej@roadley.dk>
2016-02-25 13:09:31 +0000

    Added isPortOpen

---
+++ test/isInPortOpen-test.js
@@ -0,0 +1,9 @@
+var midi = require("../midi.js");
+
+var input = new midi.input();
+
+console.log('Is open ', input.isPortOpen());
+input.openPort(0);
+console.log('Is open ', input.isPortOpen());
+
+input.closePort();

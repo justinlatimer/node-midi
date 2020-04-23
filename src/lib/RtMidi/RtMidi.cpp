96941c58b30e97aa3ada7359e8fb72446e7ce0d4
Loz Jackson <lozjackson@talktalk.net>
2020-04-23 18:25:02 +0100

    Increase buffer size

--- src/lib/RtMidi/RtMidi.cpp
+++ src/lib/RtMidi/RtMidi.cpp
@@ -2386,7 +2386,7 @@ static std::string ConvertToUTF8(const TCHAR *str)
   return u8str;
 }
 
-#define  RT_SYSEX_BUFFER_SIZE 1024
+#define  RT_SYSEX_BUFFER_SIZE 2048
 #define  RT_SYSEX_BUFFER_COUNT 4
 
 // A structure to hold variables related to the CoreMIDI API
